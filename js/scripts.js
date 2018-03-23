11aa7cbf1c4ce97255912dc346876bc1c202049e
Luke Stockburger <lstockburg@gmail.com>
2018-03-23 15:02:36 -0700

    add if/else and relevant values

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $("#quiz").submit(function(event){
     event.preventDefault();
     var score = parseInt($("input:radio[name=febe]:checked").val()) + parseInt($("input:radio[name=dof]:checked").val()) +
-    parseInt($("#build").val()) +
-    parseInt($("#lang").val());
+    parseInt($("#appeal").val()) +
+    parseInt($("#lang").val()) +
+    parseInt($("#comp").val());
+    if (score >= 5 && score <=7) {
+
+    }

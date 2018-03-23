cd374a37580ac972c73ac69b475971dd305259c8
Luke Stockburger <lstockburg@gmail.com>
2018-03-23 16:34:23 -0700

    streamline issues with backend logic. add name input for plugging into .result

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event){
     event.preventDefault();
+    var name = ($("input:text[name=person]").val())
     var score = parseInt($("input:radio[name=febe]:checked").val()) + parseInt($("input:radio[name=dof]:checked").val()) +
     parseInt($("#appeal").val()) +
     parseInt($("#lang").val()) +
     parseInt($("#comp").val());
     if (score >= 5 && score <= 7) {
       $(".result").show();
+      $("#name").text(name);
       $("#track").text("Java");
       $("#whatbuild").text("applications");
     }
     else if (score >= 8 && score <= 10) {
       $(".result").show();
+      $("#name").text(name);
       $("#track").text("Ruby");
       $("#whatbuild").text("frameworks");
     }
     else {
       $(".result").show();
+      $("#name").text(name);
       $("#track").text("C#");
       $("#whatbuild").text("software");
     }

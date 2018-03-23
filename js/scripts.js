f931a5e17051f511cf7a87104cc0efe40f3a7a81
Luke Stockburger <lstockburg@gmail.com>
2018-03-23 14:20:41 -0700

    add more backendlogic, ids, names

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event){
     event.preventDefault();
-    var score = parseInt($("input:radio[name=febe]:checked").val()) + parseInt($("input:radio[]"))
+    var score = parseInt($("input:radio[name=febe]:checked").val()) + parseInt($("input:radio[name=dof]:checked").val()) +
+    parseInt($("#build").val()) +
+    parseInt()
